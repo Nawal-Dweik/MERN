59258cdc97bde2dfcade18a1df2dc51a4701cc53
Nawal-Dweik <76469553+Nawal-Dweik@users.noreply.github.com>
2021-02-17 10:35:47 +0200

    Delete Login_and_Registration directory

--- Login_and_Registration/server/routes/auth.route.js
+++
@@ -1,6 +0,0 @@
-const AuthController = require("../controllers/auth.controller");
-
-module.exports = (app) => {
-  //app.post('/api/create', AuthController.create);
-  app.post("/api/createNewUser", AuthController.createUser);
-};
